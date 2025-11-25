f12f9fd07636432215a9418e3dc4cb56a53af390
JuanLoaiza007 <juandaloaiza723@gmail.com>
2025-11-25 17:43:58 -0500

    feat: improve summary table responsive

--- src/components/features/calc/SummarySection.jsx
+++ src/components/features/calc/SummarySection.jsx
@@ -13,37 +13,69 @@ export default function SummarySection({ summary, formData }) {
   return (
     <Card className="w-full">
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Saldo Final</TableHead>
-              <TableHead>Total invertido</TableHead>
-              <TableHead>Ganancia</TableHead>
-              <TableHead>Tasa</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                {"$"}
-                {formatterToCOP.format(summary.Balance.toFixed(2))}
-              </TableCell>
-              <TableCell>
-                {"$"}
-                {formatterToCOP.format(summary.Invested.toFixed(2))}
-              </TableCell>
-              <TableCell>
-                {"$"}
-                {formatterToCOP.format(
-                  summary.Balance.toFixed(2) - summary.Invested.toFixed(2)
-                )}
-              </TableCell>
-              <TableCell>
-                {formData.rateValue}% {formData.rateType}
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Total invertido</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  {"$"}
+                  {formatterToCOP.format(summary.Invested.toFixed(2))}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Saldo Final</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  {"$"}
+                  {formatterToCOP.format(summary.Balance.toFixed(2))}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Ganancia</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  {"$"}
+                  {formatterToCOP.format(
+                    summary.Balance.toFixed(2) - summary.Invested.toFixed(2)
+                  )}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Tasa</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  {formData.rateValue}% {formData.rateType}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
       </CardContent>
     </Card>
   );
